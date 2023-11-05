5cb84f707b27f6753a446413e6f2d17ee0425bb8
Edgar Carpinteyro <110205691+Edgar-Carpinteyro@users.noreply.github.com>
2023-11-05 15:39:32 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+//Example 1
+const body = document.querySelector("body");
+
+body.style["background-color"] = "yellow";
+
+//Example 2
+const example2 = document.querySelector("#example2");
+
+example2.style.fontSize = "24px";
+
+//Example 3
+const example3 = document.querySelector("h2");
+
+example3.style["color"] = "red";
+
+//Example 4
+const example4 = document.getElementById("example4");
+
+example4.style.borderStyle = "solid";
+example4.style.borderColor = "black";
+
+//Example 5
+const example5 = document.getElementById("example5");
+
+example5.style.backgroundImage = "url(https://images.unsplash.com/photo-1697603309921-91e3277fd05f?auto=format&fit=crop&q=80&w=2835&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)";
+
+
+//Example 6
+const example6 = document.getElementById("example6");
+example6.style.display = "none";
+
+//Example 7
+const example7 = document.querySelector("ul");
+example7.style.color = "green";
